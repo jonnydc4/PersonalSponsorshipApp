d981e2d6710704b45102bc956451ab4f76ce5a11
Jon <jonnydc4@gmail.com>
2023-09-15 19:08:38 -0600

    Express app runs now

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+// Import the express library
+const express = require('express');
+
+// Initialize the express application
+const app = express();
+
+// Define a simple route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server on port 3000
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}`);
+});
 
