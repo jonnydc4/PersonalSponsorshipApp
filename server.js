e6a8e0e407b9aa2b1c460d2c384cd2d6d6c2317c
Koliosis <kstevendavis@gmail.com>
2023-10-12 18:56:00 -0600

    allJobs endpoint working

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // Import the express library
 const express = require('express');
 const {Client} = require("pg");
+const cors = require('cors');
+// Use CORS middleware and allow any domain to access your API
+
 
 // Initialize the express application
 const app = express();
+app.use(cors());
 
 // Create a connection to the database
 const dbClient = new Client({
@@ -12,22 +16,30 @@ const dbClient = new Client({
     host: "postgres",
 });
 
-// This makes our "/" endpoint render our react app
-app.use(express.static('client/build'));
+dbClient.connect()
+    .then(() => console.log('Connected to the database'))
+    .catch(err => console.error('Connection error', err.stack));
 
-app.get("/allJobs"), async (req, res) => {
+// This makes our "/" endpoint render our react app
+// app.use(express.static('client/build'));
+
+app.get("/allJobs", async (req, res) => {
+    try {
     const queryResults = await dbClient
-        .query("Select * From jobs")
+        .query("Select * FROM public.jobs")
         .then((payload) => {
             return payload.rows;
         })
-        .catch(() => {
-            throw new Error("Query failed");
-        });
     res.setHeader("Content-Type", "application/json");
     res.status(200);
     res.send(JSON.stringify(queryResults));
-}
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500);
+        res.send(err);
+    }
+})
 
 // Start the server on port 3000
 const port = 3000;
