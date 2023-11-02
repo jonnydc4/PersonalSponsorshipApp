ab0bb5cae3fc3c9e83a5e765194799b0b731080b
AzureAsteroid2 <77765010+AzureAsteroid2@users.noreply.github.com>
2023-11-02 16:38:45 -0600

    Added simple commonFrame and commented out example

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,10 @@ import Button from './components/Button';
 import Jobs from './components/Jobs';
 import Login from './components/Login';
 import JobPostingForm from './components/JobPostingForm';
+import CommonFrame from './components/CommonForm';
 
 function App() {
+
     return (
         // Use the Router component to handle different routes in the app
         <Router>
@@ -21,5 +23,33 @@ function App() {
         </Router>
     );
 }
+
+
+// example that can be inserted into App (The Links do not load anything besides the new page. This will always throw
+// a Get error when it loads the page.)
+/*
+const items = [
+        { name: 'Login', link: '/login' },
+        { name: 'Post Job', link: '/post-job' },
+        { name: 'Jobs', link: '/jobs' },
+        // ... add more items as needed
+    ];
+    return (
+        // Use the Router component to handle different routes in the app
+        <Router>
+            <div style={{ textAlign: 'center' }}>
+
+                <CommonFrame items={items}>
+                    <Routes>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/post-job" element={<JobPostingForm />} />
+                        <Route path="/jobs" element={<Jobs />} />
+                        <Route path="/" element={<Button label="Companies" />} />
+                    </Routes>
+                </CommonFrame>
+            </div>
+        </Router>
+    );
+ */
 
 export default App;
