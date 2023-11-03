5d5fa09108308661d1bee34c11e4b7d9bc11022d
AzureAsteroid2 <77765010+AzureAsteroid2@users.noreply.github.com>
2023-11-02 20:22:38 -0600

    commonframe has nav bar and selectable text menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,37 @@
-// client/src/App.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Button from './components/Button';
-import Jobs from './components/Jobs';
+import CommonFrame from './components/CommonFrame';
 import Login from './components/Login';
 import JobPostingForm from './components/JobPostingForm';
-import CommonFrame from './components/CommonForm';
+import Jobs from './components/Jobs';
+import Button from './components/Button';
 
 function App() {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/allJobs');
+                const data = await response.json();
+                const formattedItems = data.map(job => ({
+                    name: job.title,
+                    description: job.description,
+                    location: job.location,
+                    // ... other job properties you want to display
+                }));
+                setItems(formattedItems);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
-        // Use the Router component to handle different routes in the app
         <Router>
             <div style={{ textAlign: 'center' }}>
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/post-job" element={<JobPostingForm />} />
-                    <Route path="/jobs" element={<Jobs />} />
-                    <Route path="/" element={<Button label="Companies" />} />
-                </Routes>
-            </div>
-        </Router>
-    );
-}
-
-
-// example that can be inserted into App (The Links do not load anything besides the new page. This will always throw
-// a Get error when it loads the page.)
-/*
-const items = [
-        { name: 'Login', link: '/login' },
-        { name: 'Post Job', link: '/post-job' },
-        { name: 'Jobs', link: '/jobs' },
-        { name: 'fake'},
-        // ... add more items as needed
-    ];
-    return (
-        // Use the Router component to handle different routes in the app
-        <Router>
-            <div style={{ textAlign: 'center' }}>
-
                 <CommonFrame items={items}>
                     <Routes>
                         <Route path="/login" element={<Login />} />
@@ -51,6 +43,56 @@ const items = [
             </div>
         </Router>
     );
- */
+}
 
 export default App;
+
+// example that can be inserted into App (The Links do not load anything besides the new page. This will always throw
+// a Get error when it loads the page.)
+/*
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CommonFrame from './components/CommonFrame';
+import Login from './components/Login';
+import JobPostingForm from './components/JobPostingForm';
+import Jobs from './components/Jobs';
+import Button from './components/Button';
+
+function App() {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        // Fetch data from /allJobs
+        fetch('/allJobs')
+            .then(response => response.json())
+            .then(data => {
+                // Convert the fetched data to the desired items structure
+                const formattedItems = data.map(job => ({
+                    name: job.job_name, // Assuming the job has a property called 'job_name'
+                    link: `/jobs/${job.id}` // Assuming each job has a unique 'id'
+                }));
+                setItems(formattedItems);
+            })
+            .catch(error => {
+                console.error('Error fetching jobs:', error);
+            });
+    }, []); // The empty dependency array ensures this useEffect runs once when the component mounts
+
+    return (
+        <Router>
+            <div style={{ textAlign: 'center' }}>
+                <CommonFrame items={items}>
+                    <Routes>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/post-job" element={<JobPostingForm />} />
+                        <Route path="/jobs" element={<Jobs />} />
+                        <Route path="/" element={<Button label="Companies" />} />
+                    </Routes>
+                </CommonFrame>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
+ */
---
+++ client/src/components/CommonFrame.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const CommonFrame = ({ items }) => {
+    // State to keep track of the currently selected item
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+
+            {/* Navigation Pane */}
+            <div style={{ padding: '10px', borderBottom: '1px solid black' }}>
+                Navigation Pane Content Here
+            </div>
+
+            {/* Content Area */}
+            <div style={{ display: 'flex', flex: 1 }}>
+                {/* Selectable List */}
+                <div style={{ flex: 1, borderRight: '1px solid black', overflowY: 'auto' }}>
+                    {items.map((item, index) => (
+                        <div key={index} onClick={() => setSelectedItem(item)}>
+                            {item.name}
+                        </div>
+                    ))}
+                </div>
+
+                {/* Payload/Main Area */}
+                <div style={{ flex: 2, padding: '20px' }}>
+                    {selectedItem ? (
+                        <div>
+                            <h2>{selectedItem.name}</h2>
+                            {Object.entries(selectedItem).map(([key, value]) => {
+                                // Exclude the 'id' property
+                                if (key !== 'id') {
+                                    return (
+                                        <div key={key}>
+                                            <strong>{key}:</strong> {value}
+                                        </div>
+                                    );
+                                }
+                                return null;
+                            })}
+                        </div>
+                    ) : (
+                        <p>Select an item from the list to view its details.</p>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CommonFrame;
+
+
