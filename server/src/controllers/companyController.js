17674e5ac9eece63313fbee1b3ae6f23974b4b2e
Morgan Molyneaux <92762025+MorganMolyneaux22@users.noreply.github.com>
2024-02-21 13:15:13 -0700

    Updating companyController.js
    Further documentation.  Added header comment and explained further what method does.

--- server/src/controllers/companyController.js
+++ server/src/controllers/companyController.js
@@ -1,5 +1,11 @@
+// <-------- companyController.js - contains all controllers used in company side operations -------->
+// Company controller acts as the middleman between the routes and the model.
+// It is responsible for handling the business logic and passing the data to the model.
+
 const model = require("../models/model");
 
+// Fetches all companies and awaits the response from the model. If successful, returns the data (all companies within the database).
+// For further details, see model.js
 const allCompanies = async () => {
     return await model.getAllCompanies()
 }
