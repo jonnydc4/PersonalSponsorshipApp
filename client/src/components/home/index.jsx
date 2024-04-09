89d84363335464ff23d598ecd72bd3b2d3f2a788
Morgan Molyneaux <92762025+MorganMolyneaux22@users.noreply.github.com>
2024-04-09 17:05:28 -0600

    Adding buttons to Home Page
    Buttons used to navigate to other pages (still need to route correctly using sx hidden tag in MUI).

--- client/src/components/home/index.jsx
+++ client/src/components/home/index.jsx
@@ -1,21 +1,48 @@
-import React from 'react'
-import { useAuth } from '../../contexts/authContext'
+import React from 'react';
+import { useAuth } from '../../contexts/authContext';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 import Typography from "@mui/material/Typography";
+import Button from '@mui/material/Button';
+import { Link as RouterLink } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
 
 const Home = () => {
-    const { currentUser } = useAuth()
-    return (
-        <Typography
-            variant="h4"
-            component="div"
-            sx={{
-                fontWeight: 'bold',
-                pt: 14,
-            }}
-        >
-            Hello {currentUser.displayName || currentUser.email}, you are now logged in.
-        </Typography>
-    )
-}
+    const { currentUser } = useAuth();
 
-export default Home
+    return (
+        <AppBar position="fixed" color="default" sx={{ zIndex: 1201 }}>
+        <Toolbar>
+            <Typography fontWeight="fontWeightMedium" variant="h5">
+                Influinity
+            </Typography>
+        
+            <Box sx={{ flexGrow: 1 }} />
+            <Button fontWeight="fontWeightMedium" variant="h6" sx={{ pl: 1 }} component={RouterLink} to="/home">
+                Home
+            </Button>
+            <Button fontWeight="fontWeightMedium" variant="h6" sx={{ pl: 1 }} component={RouterLink} to="/job-manager">
+                Job Listings
+            </Button>
+            <Button fontWeight="fontWeightMedium" variant="h6" sx={{ pl: 1 }} component={RouterLink} to="/home">
+                Messanges
+            </Button>
+            <Button color="primary" component={RouterLink} to="/profile">
+                <Avatar sx={{ width: 24, height: 24 }} src={currentUser.photoURL} />
+            </Button>
+
+        </Toolbar>   
+        <Box sx={{ pt: 14, pl: 2, pr: 2 }}>
+            <Typography variant="h4" component="div" sx={{ fontWeight: 'bold' }}>
+                Welcome, {currentUser.displayName || currentUser.email}
+            </Typography>
+            
+
+            
+        </Box>
+        </AppBar>
+    );
+};
+
+export default Home;
