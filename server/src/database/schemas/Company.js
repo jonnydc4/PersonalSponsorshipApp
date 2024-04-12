a341d577c4c74104e28fe7d56092bf4da60a3cfa
Jon <jonnydc4@gmail.com>
2024-04-11 22:25:49 -0600

    Changed _id to id in the company and influencer schemas.

--- server/src/database/schemas/Company.js
+++ server/src/database/schemas/Company.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
     const companySchema = new mongoose.Schema({
-        _id: { type: String, required: true, unique: true },
+        id: { type: String, required: true, unique: true },
         name: { type: String, required: true },
         address: String
     });
--- server/src/database/schemas/Influencer.js
+++ server/src/database/schemas/Influencer.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
     const influencerSchema = new mongoose.Schema({
-        _id: { type: String, required: true, unique: true },
+        id: { type: String, required: true, unique: true },
         firstName: { type: String, required: true },
         lastName: { type: String, required: true },
         userName: { type: String, required: true }
