2d0b25a5845d3f710cbc9ef8bb2f56dd5a41b681
Jon <jonnydc4@gmail.com>
2024-04-21 23:54:23 -0600

    Added create and get functions for messageRoom.js

--- server/src/database/schemas/Message.js
+++ server/src/database/schemas/Message.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const messageSchema = new mongoose.Schema({
     content: { type: String, required: true },
     date: { type: Date, default: Date.now }, // Uses the current date and time
-    sender: { type: String, required: true } // Identifier for sender type
+    sender: { type: String, required: true } // Identifier for sender type ex. user id
 });
 
 module.exports = messageSchema;
--- server/src/database/schemas/MessageRoom.js
+++ server/src/database/schemas/MessageRoom.js
@@ -5,12 +5,10 @@ const messageSchema = require('./Message');
 const messageRoomSchema = new mongoose.Schema({
     company: {
         type: String,
-        ref: 'Company',
         required: true
     },
     influencer: {
         type: String,
-        ref: 'Influencer',
         required: true
     },
     messages: [messageSchema] // Embedded array of Message documents
