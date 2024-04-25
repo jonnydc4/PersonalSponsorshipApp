a796e72c9ed892d0be4de8f3e48e69c787211ecf
Morgan Molyneaux <92762025+MorganMolyneaux22@users.noreply.github.com>
2024-04-24 21:15:17 -0600

    Adding placeholder for photo on welcomeboard

--- client/src/components/home/WelcomeBoard.jsx
+++ client/src/components/home/WelcomeBoard.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Typography, Badge } from '@mui/material';
+import { Toolbar, Typography, Box, Drawer, Tabs, Tab, Badge, Skeleton, Avatar } from '@mui/material';
 import WavingHandIcon from '@mui/icons-material/WavingHand';
 
 function WelcomeBoard({ userName }) {
     // Used to display a welcome message to the user - can be customized as it's within its
     // own component.  Therefore, you can add more elements to this component without messing up homepage.
   return (
-    <Typography variant="h4" sx={{ fontWeight: 'bold', marginLeft: 2, marginBottom: 5 }}> {/* You can modify padding here */}
-      Welcome, {userName} <Badge color="primary">
-        <WavingHandIcon color="action" fontSize="large" />
-      </Badge>
-    </Typography>
+    <Box sx={{ display: 'flex', alignItems: 'center', marginLeft: 2, marginBottom: 5 }}>
+      <Avatar alt="add description here" src="/add/source/here" sx={{ marginRight: 2, marginTop: 2, width: 56, height: 56}} />
+      <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
+        Welcome, {userName} <Badge color="primary">
+          <WavingHandIcon color="action" fontSize="large" />
+        </Badge>
+      </Typography>
+    </Box>
   );
 }
 
